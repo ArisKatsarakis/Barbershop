2fae8681b0d28073469b45d23a753d5675e2aa2c
diaolos <ariskatsarkis@hotmail.com>
2022-08-24 11:00:15 +0300

    FrontEnd Embedded

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<dash-board></dash-board>
+</template>
+
+<script>
+import DashBoard from "@/components/DashBoard";
+export default {
+  name: 'App',
+  components: {
+    DashBoard,
+
+  },
+  data() {
+    return {
+      msg: "Test",
+    }
+  },
+  mounted() {
+    fetch("/api/v1/appointment")
+    .then((response) => response.text())
+    .then( (data) => {
+      this.msg = data;
+    });
+  }
+}
+</script>
+
+<style>
+
+</style>
