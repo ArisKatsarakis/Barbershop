ad557ca968290f1248c5fc7543d08f24d6e27ea7
KatsarakisAris <ariskatsarakis@hotmail.com>
2023-06-18 10:44:56 +0300

    Add devTools Starter module.Add User Class

---
+++ src/main/java/com/example/demo/User/BarbershopUser.java
@@ -0,0 +1,75 @@
+package com.example.demo.User;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class BarbershopUser {
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+    public BarbershopUser(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public BarbershopUser() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
