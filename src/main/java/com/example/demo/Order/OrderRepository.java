ad11ad3154d87ac425e52c337071cf5eaf857413
KatsarakisAris <ariskatsarakis@hotmail.com>
2023-06-28 14:24:04 +0300

    Add function to repository for fetching by Customer Id

--- src/main/java/com/example/demo/Order/OrderRepository.java
+++ src/main/java/com/example/demo/Order/OrderRepository.java
@@ -2,9 +2,11 @@ package com.example.demo.Order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<SalesOrder,Long> {
     Optional<SalesOrder> findById(Long id );
+    List<SalesOrder> findByCustomerId(Long customerdId);
 
 }
